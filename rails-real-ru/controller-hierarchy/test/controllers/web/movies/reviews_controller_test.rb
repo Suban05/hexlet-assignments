096e8a902adac6bab059e9e88ef3eecea449b946
@hexlet/cli <support@hexlet.io>
2024-12-11 23:51:28 +0500

    submit rails-real-ru/controller-hierarchy

---
+++ rails-real-ru/controller-hierarchy/test/controllers/web/movies/reviews_controller_test.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class Web::Movies::ReviewsControllerTest < ActionDispatch::IntegrationTest
+end
---
+++ rails-real-ru/controller-hierarchy/test/controllers/web/reviews_controller_test.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class Web::ReviewsControllerTest < ActionDispatch::IntegrationTest
+  test 'should get index' do
+    assert true
+  end
+end
