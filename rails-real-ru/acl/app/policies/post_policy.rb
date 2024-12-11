80cae1c97d0db2657624d4e3ca6b8e8303ec2506
@hexlet/cli <support@hexlet.io>
2024-12-11 23:22:33 +0500

    submit rails-real-ru/acl

--- rails-real-ru/acl/app/policies/post_policy.rb
+++ rails-real-ru/acl/app/policies/post_policy.rb
@@ -2,6 +2,34 @@
 
 class PostPolicy < ApplicationPolicy
   # BEGIN
-  
+  def new?
+    user
+  end
+
+  def create?
+    user
+  end
+
+  def edit?
+    admin? || author?
+  end
+
+  def update?
+    admin? || author?
+  end
+
+  def destroy?
+    admin?
+  end
+
+  private
+
+  def author?
+    record.author == user
+  end
+
+  def admin?
+    user&.admin?
+  end
   # END
 end
--- rails-real-ru/acl/test/controllers/home_controller_test.rb
+++ rails-real-ru/acl/test/controllers/home_controller_test.rb
@@ -4,6 +4,7 @@ require 'test_helper'
 
 class HomesControllerTest < ActionDispatch::IntegrationTest
   test 'should get index' do
-    assert true
+    get root_url
+    assert_response :success
   end
 end
